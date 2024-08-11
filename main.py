51eec00fe7ed3e3b853f41a487fa374004742a94
Mikhail Polyakov <timadgi@gmail.com>
2024-08-11 20:51:05 +0500

    corrected main

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ def main():
     params = config()
     database_name = "hh"  #Название базы данных
 
-    # hh_api = HeadHunterRuAPI    #Получение данных по API
-    # companies = hh_api.getting_info_company()   #Получение данных по компаниям
-    # vacancies = hh_api.getting_vacancy()    #Получение данных по вакансиям
-    #
-    # # Создание базы данных и таблиц
-    # hh_database = DataBase
-    # hh_database.create_database(database_name, params)
-    #
-    # # Внесение данных в таблицу "company"
-    # hh_database.save_data_to_database_company(companies, database_name, params)
-    # # Внесение данных в таблицу "vacancy"
-    # hh_database.save_data_to_database_vac(vacancies, database_name, params)
+    hh_api = HeadHunterRuAPI    #Получение данных по API
+    companies = hh_api.getting_info_company()   #Получение данных по компаниям
+    vacancies = hh_api.getting_vacancy()    #Получение данных по вакансиям
+
+    # Создание базы данных и таблиц
+    hh_database = DataBase
+    hh_database.create_database(database_name, params)
+
+    # Внесение данных в таблицу "company"
+    hh_database.save_data_to_database_company(companies, database_name, params)
+    # Внесение данных в таблицу "vacancy"
+    hh_database.save_data_to_database_vac(vacancies, database_name, params)
 
     # Работа с БД
     db_manager = DBManager(database_name, params)
